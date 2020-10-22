25008c0e2b8c00672c044dc35cf334560c9b8514
taslimR <hridoy.bubd@gmail.com>
2020-10-22 09:37:34 +0600

    updated

---
+++ app/src/main/java/com/example/test_opencv/MrzResponseActivity.java
@@ -0,0 +1,13 @@
+package com.example.test_opencv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MrzResponseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mrz_response);
+    }
+}
